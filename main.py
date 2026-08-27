8d842a32a8be7cd9bacfd9c190a93a3da14d3623
rubaiiyat <abirr1516@gmail.com>
2026-08-27 17:51:07 +0600

    feat: add gaussian noise simulation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
+
 
 image = cv2.imread("dataset/rural_area.jpg")
 
@@ -9,8 +11,26 @@ else:
     print("Image loaded successfully!")
     print("Image size:", image.shape)
 
+
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+   
+    noise = np.random.normal(0, 25, image_rgb.shape)
+
+    noisy_image = image_rgb + noise
+
+    noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
     plt.imshow(image_rgb)
+    plt.title("Original Image")
     plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(noisy_image)
+    plt.title("Noisy Image")
+    plt.axis("off")
+
     plt.show()
