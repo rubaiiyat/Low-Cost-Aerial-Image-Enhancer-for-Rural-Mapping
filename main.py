f2bccb08cd61143fe6b330aff9d9206ddbd97bbd
rubaiiyat <abirr1516@gmail.com>
2026-08-27 17:56:44 +0600

    feat: compare denoising methods using PSNR

--- main.py
+++ main.py
@@ -25,8 +25,21 @@ else:
     noisy_psnr = cv2.PSNR(image_rgb, noisy_image)
     denoised_psnr = cv2.PSNR(image_rgb, denoised_image)
 
+   
+    gaussian_denoised = cv2.GaussianBlur(noisy_image, (5, 5), 0)
+
+ 
+    bilateral_denoised = cv2.bilateralFilter(noisy_image, 9, 75, 75)
+
+   
+    median_psnr = cv2.PSNR(image_rgb, denoised_image)
+    gaussian_psnr = cv2.PSNR(image_rgb, gaussian_denoised)
+    bilateral_psnr = cv2.PSNR(image_rgb, bilateral_denoised)
+
     print("PSNR of noisy image:", noisy_psnr)
-    print("PSNR of denoised image:", denoised_psnr)
+    print("PSNR of median filter:", median_psnr)
+    print("PSNR of gaussian filter:", gaussian_psnr)
+    print("PSNR of bilateral filter:", bilateral_psnr)
     plt.figure(figsize=(15, 5))
 
     plt.subplot(1, 3, 1)
