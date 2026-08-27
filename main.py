8b945e2ad7ffb54fda8d59b53c1252450bd8146d
rubaiiyat <abirr1516@gmail.com>
2026-08-27 17:53:51 +0600

    feat: remove noise using median filter

--- main.py
+++ main.py
@@ -20,17 +20,22 @@ else:
     noisy_image = image_rgb + noise
 
     noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+    denoised_image = cv2.medianBlur(noisy_image, 5)
+    plt.figure(figsize=(15, 5))
 
-    plt.figure(figsize=(12, 5))
-
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.imshow(image_rgb)
     plt.title("Original Image")
     plt.axis("off")
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.imshow(noisy_image)
     plt.title("Noisy Image")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(denoised_image)
+    plt.title("Denoised Image")
     plt.axis("off")
 
     plt.show()
